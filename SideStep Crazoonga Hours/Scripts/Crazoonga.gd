340b7c00415ce21e732de39d24c1d64a29677d3c
EggnogBaileys <malonemast@gmail.com>
2023-09-27 12:18:33 -0400

    Attempted knockback. Failed.

--- SideStep Crazoonga Hours/Scripts/Crazoonga.gd
+++ SideStep Crazoonga Hours/Scripts/Crazoonga.gd
@@ -9,7 +9,6 @@ extends CharacterBody3D
 
 var canSwing = true
 
-
 @export var maxWalkSpeed = 1200.0
 @export var walkSpeed = 1200.0
 @export var turnSpeed = 5.0
@@ -18,19 +17,25 @@ var canSwing = true
 # With each shell found, crab can take one extra hit.
 var shells = 0
 
-
-
+var knockback = Vector3.ZERO
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var canMove = true
 
 
 func _physics_process(delta):
 	
-	SideStep(delta)
-	Rotation(delta)
+	if not is_on_floor():
+		velocity.y -= gravity * delta	
+	
+	if canMove:
+		SideStep(delta)
+		Rotation(delta)
 	
 	if Input.is_action_pressed("ClawSwing") and canSwing:
 		Swing()
+		
+	Knockback(delta)
 	
-
 
 
 func SideStep(delta):
@@ -47,6 +52,7 @@ func SideStep(delta):
 		legAnimator.play("Idle")
 	
 	move_and_slide()
+
 func Rotation(delta):
 		
 	var rotation_direction = 0
@@ -60,16 +66,33 @@ func Rotation(delta):
 	else:
 		walkSpeed = maxWalkSpeed
 
-
 func Swing():
 	canSwing = false
 	swingTimer.start()
 	clawAnimator.play("Swing")
 	$ClawSwing/Swing.play("Swing")
-
 func _on_swing_timer_timeout():
-	canSwing = true
-	
+	canSwing = true	
 func _on_claw_animation_finished(anim_name):
 	if anim_name == "Swing":
 		clawAnimator.play("Default")
+
+
+func Knockback(delta):
+	#knockback = velocity.move_toward(Vector3.ZERO, delta * 200)
+	#knockback = move_and_collide(knockback)
+	pass
+
+
+
+
+
+
+func _on_crazoonga_area_3d_area_entered(area):
+	if area.name == "EnemyHurtbox":
+		canMove = false
+		print("are we even colliding")
+		velocity = (global_transform.origin - area.global_transform.origin).normalized()
+		velocity *= -10
+		await get_tree().create_timer(0.8). timeout
+		canMove = true
