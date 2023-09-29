6f8d46972998d31a7c53db1f6bd3ab3749227678
EggnogBaileys <malonemast@gmail.com>
2023-09-29 09:59:13 -0400

    Added Lobster Body (still need hole that it resides in), tweaked some scripts.

--- Scripts/Crazoonga.gd
+++ Scripts/Crazoonga.gd
@@ -11,7 +11,7 @@ const MAX_FALL_SPEED := -80.0
 @onready var clawAnimator = $Crazoonga/Claw
 @onready var legAnimator = $Crazoonga/Legs
 
-var canSwing = true
+@export var canSwing = true
 
 @export var walkSpeed = 10.0
 @export var turnSpeed = 5.0
@@ -26,34 +26,30 @@ var knockback := Vector3.ZERO
 var direction: float
 
 func _physics_process(delta):
-	# Only multiply by delta if the velocity is modified (+=, -=...)
-	# Do not multiply by delta if the velocity is simply assigned with =
-	# Ternary operator, equivalent to "if test: x = true; else: x = false"
+
 	velocity = SideStep() if $Knockback.is_stopped() else knockback
 	if Input.is_action_pressed("ClawSwing") and canSwing:
 		Swing()
+
 	velocity.y -= gravity * delta \
 		if not is_on_floor() or velocity.y < MAX_FALL_SPEED \
 		else 0
 	rotate_y(Input.get_axis("ui_right", "ui_left") * turnSpeed * delta)
 	move_and_slide()
-
+	
 
 func SideStep() -> Vector3:
-	# The most compact way to get the direction of inputs
+
 	direction = Input.get_axis("ui_down", "ui_up") * walkSpeed
-	# There are more flexible ways to setup animations, but this works for now
+
 	if direction:
 		legAnimator.play("Walk")
 	else:
 		legAnimator.play("Idle")
-	# Using basis manipulation to get the desired movement
-	# I could have used Euler angles, or Quaternions, if desired
-	# As a matter of style, I prefer more functional code that does
-	# not modify "velocity" outside of "_physics_process"
+
 	return Vector3(
-		transform.basis.z.x * direction,
-		velocity.y,
+		transform.basis.z.x * direction, 
+		velocity.y, 
 		transform.basis.z.z * direction
 	)
 
@@ -73,25 +69,12 @@ func _on_claw_animation_finished(anim_name):
 	if anim_name == "Swing":
 		clawAnimator.play("Default")
 
-# You can connect multiple signals to the same function
-# You can also name your connected functions whatever you want
-#
-# Notice how the function doesn't check who touched it, and only runs when
-# walking on damaging objects. The CrazoongaArea3D has its collision mask set
-# to 4, which I have named "hazard", and $EnemyHurtbox has its collision
-# layer set to 4, "hazard" also
-# Think of a collision mask as "reading all physics objects that affect this
-# layer," and a collision layer as "affecting the layer."
-func _on_damage_taken(by: Area3D):
-	# Cooldown after damage is taken
+func _on_damage_taken(hazard: Node3D):
 	if not $Knockback.is_stopped():
-		# Notice how instead of nesting all code below inside an "if",
-		# it stops here, leaving us a lot more indentation space
 		return
 	$Knockback.start()
 
-	# Calculate the desired angle of the knockback
-	knockback = global_transform.origin - by.global_transform.origin
+	knockback = global_transform.origin - hazard.global_transform.origin
 	knockback = knockback.normalized() * H_KNOCKBACK_FORCE
 	knockback.y = V_KNOCKBACK_FORCE
 
@@ -99,7 +82,11 @@ func _on_damage_taken(by: Area3D):
 	# PhysicsBody-related activities should be synced to physics frames
 	# Notice how the $Knockback timer's process callback is "Physics", too
 	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	# Slowly stop horizontal knockback and ground vertical knockback
+
 	tween.tween_property(self, "knockback", Vector3(0,-10, 0), $Knockback.wait_time)\
 			.set_trans(Tween.TRANS_SINE)\
 			.set_ease(Tween.EASE_OUT)
+			
+			
+			
+# In Unity we refer to ourself through "this", in Godot we use "self".
--- Scripts/Lobster.gd
+++ Scripts/Lobster.gd
@@ -1,62 +1,40 @@
 extends MeshInstance3D
 
-# The hole is gone! Moved to Hole.gd. The lobster doesn't need the hole to survive
-# This is way more flexible and maintainable, read Hole.gd for more info
 
-# Read the comment in _on_rock_throw_timer_timeout()
 signal throw_rock
 
 var health: int = 3
-# Declare variables used every frames in global scope to save performances
-# Also give it a type, types are good
+
 var players: Array[Node3D]
 
 
-# Put underscores before unused arguments
+
 func _process(_delta):
-	# $PlayerDetection will only detect the player, Read the comment
-	# of _on_lobster_hurtbox_area_entered() for more info
 	# Areas can return an array of everything inside of them
 	# Note that Static/Rigid/CharacterBodies are bodies, and Areas are areas
 	players = $PlayerDetection.get_overlapping_bodies()
 	# If the array that can only contain players is not empty,
 	# Then the first element is the player
-	# No need to set or keep track of any global variables, and you can test
-	# the lobster's scene by itself
 	if not players.is_empty():
 		look_at(players[0].global_position)
 
 
-# Notice how the function doesn't check who touched it, and only runs whent
-# the claw snaps the lobster. The $LobsterHurtbox has its collision mask set
-# to 3, which I have named "claw", and the crab's $ClawSwing has its collision
-# layer set to 3, "claw" also
-# Think of a collision mask as "reading all physics objects that affect this
-# layer," and a collision layer as "affecting the layer."
 func _on_lobster_hurtbox_area_entered(_area):
-	# Use print_debug instead. They say where they come from,
-	# and they don't run if you forget to remove them
-	# This matters, because print statements are surprisingly costly
-	# in terms of resources
 	print_debug("Ouch")
 	health -= 1
 	if health < 1:
 		queue_free()
 
 
-# The lobster shouldn't care if a rock spawns or not,
-# since the rocks become children of the lobster's parent
-# Use a signal instead, and connect it at the best place
+
 func _on_rock_throw_timer_timeout():
 	emit_signal("throw_rock")
 
 
-# No need to check the body that triggered the singal
-# Read the comment on function _on_lobster_hurtbox_area_entered()
+
 func _on_player_detection_body_entered(_body):
 	$RockThrowTimer.start()
 
 
-# Same as above
 func _on_player_detection_body_exited(_body):
 	$RockThrowTimer.stop()
